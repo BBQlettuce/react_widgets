8546d14bc333c3a249e017b9c14a521e046d12b8
Oscar Liu <oscar.liu.13q@gmail.com>
2015-12-29 18:22:25 -0500

    bundle, create widgets.jsx

--- widgets.jsx
+++ widgets.jsx
@@ -1,2 +1,14 @@
 var React = require('react');
 var ReactDOM = require('react-dom');
+
+var Widgets = React.createClass({
+  render: function() {
+    return(
+      <div>widget goes here</div>
+    );
+  }
+});
+
+document.addEventListener("DOMContentLoaded", function() {
+  ReactDOM.render(<Widgets />, document.getElementById('main'));
+});
