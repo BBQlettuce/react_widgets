1272feb29da4ad20e6ab0e63f6b6aef845b10858
Oscar Liu <oscar.liu.13q@gmail.com>
2016-01-12 23:13:07 -0500

    start clock

--- weatherclock.jsx
+++ weatherclock.jsx
@@ -1,11 +1,23 @@
 var React = require('react');
 
 var Clock = react.createClass({
+  getInitialState: function() {
+    return { this.time = new Date() }
+  },
 
+  render: function() {
+
+  }
 });
 
 var Weather = react.createClass({
+  getInitialState: function() {
 
+  },
+
+  render: function() {
+
+  }
 });
 
 module.exports{
